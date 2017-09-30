11c44440b2bae836ae999287d6be567c5ec8f320
BOMXP\bomxp <son.nguyen.and@gmail.com>
2017-10-01 00:19:28 +0700

    updated khoi dong UI

--- MyApplication2/app/src/main/java/com/vinh/alarmclockandroid/activities/KhoiDong.java
+++ MyApplication2/app/src/main/java/com/vinh/alarmclockandroid/activities/KhoiDong.java
@@ -5,22 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.vinh.alarmclockandroid.R;
 
 public class KhoiDong extends AppCompatActivity {
 
-    Button batdau_btn;
+    //Button batdau_btn;
+    ImageView batdau_iv;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_khoi_dong);
+        setContentView(R.layout.activity_khoidong_blue);
 
         intent = new Intent(getApplicationContext(), MainActivity.class);
-        batdau_btn = (Button) findViewById(R.id.batdau_btn);
-        batdau_btn.setOnClickListener(new View.OnClickListener() {
+
+        batdau_iv = (ImageView) findViewById(R.id.iv_setting_blue_icon);
+
+        //batdau_btn = (Button) findViewById(R.id.batdau_btn);
+        batdau_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(intent);
